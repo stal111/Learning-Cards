71530a73674105f5dd909cce81e3a33f1339e1f0
stal111 <stal111@gmx.de>
2022-10-03 20:34:04 +0200

    TrainingScreen stuff

---
+++ lib/button/animated_button.dart
@@ -0,0 +1,45 @@
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart' as material;
+import 'package:learning_cards/card_list.dart';
+
+typedef StatusCallback = void Function(Status);
+
+class AnimatedButton extends AnimatedWidget {
+  AnimationController controller;
+  late Animation<double> animation;
+  Status status;
+  bool padding;
+
+  StatusCallback onPressed;
+
+  AnimatedButton(
+      {super.key,
+      required this.controller,
+      required this.status,
+      this.padding = false,
+      required this.onPressed})
+      :super(
+            listenable:
+                Tween<double>(begin: 34.0, end: 44.0).animate(controller)) {
+    animation = listenable as Animation<double>;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: padding
+            ? EdgeInsets.all(40 - (animation.value - 34) / 2)
+            : EdgeInsets.zero,
+        child: material.MouseRegion(
+          onEnter: (event) => controller.forward(),
+          onExit: (event) => controller.reverse(),
+          child: IconButton(
+              icon: Icon(status.icon),
+              style: ButtonStyle(
+                  iconSize: ButtonState.all(animation.value),
+                  foregroundColor: ButtonState.resolveWith((states) =>
+                      states.isHovering ? CardList.statusToColor[status.index] : Colors.grey.toAccentColor())),
+              onPressed: () => onPressed(status)),
+        ));
+  }
+}
--- lib/train_screen.dart
+++ lib/train_screen.dart
@@ -1,10 +1,67 @@
-import 'package:fluent_ui/fluent_ui.dart';
+import 'dart:ffi';
+import 'dart:ui';
 
-class TrainScreen extends StatelessWidget {
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/material.dart' as material;
+import 'package:learning_cards/button/animated_button.dart';
+import 'package:learning_cards/card_list.dart';
+
+class TrainScreen extends StatefulWidget {
   const TrainScreen({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => TrainScreenState();
+}
+
+class TrainScreenState extends State<TrainScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller1;
+  late AnimationController _controller2;
+  late AnimationController _controller3;
+
+  int cards = 7;
+  int finishedCards = 0;
+
+  double _progress = 0.0;
+
+  @override
+  initState() {
+    super.initState();
+
+    _controller1 = AnimationController(
+        duration: const Duration(milliseconds: 100), vsync: this);
+    _controller2 = AnimationController(
+        duration: const Duration(milliseconds: 100), vsync: this);
+    _controller3 = AnimationController(
+        duration: const Duration(milliseconds: 100), vsync: this);
+
+    //_animation = CurvedAnimation(parent: _controller, curve: Curves.bounceOut);
+    //_animation = Tween<double>(begin: 34.0, end: 44.0).animate(_controller);
+
+    // _controller.forward();
+  }
+
+  @override
+  dispose() {
+    _controller1.dispose();
+    _controller2.dispose();
+    _controller3.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    callback(status) => {
+          setState(() {
+            finishCard();
+
+            if (_progress >= 1.0) {
+              Navigator.pop(context);
+            }
+          })
+        };
+
     return Container(
       child: Container(
         color: Colors.white,
@@ -12,17 +69,42 @@ class TrainScreen extends StatelessWidget {
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Expanded(child: Text("Test")),
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: material.LinearProgressIndicator(
+                    value: _progress, minHeight: 20.0)),
+            Expanded(child: Text('$finishedCards/$cards       (${(_progress * 100).round()}%)', style: TextStyle(fontSize: 20),)),
             FilledButton(
-                child: const Text("Back"), onPressed: () => Navigator.pop(context)),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              IconButton(icon: const Icon(FluentIcons.emoji_disappointed), style: ButtonStyle(iconSize: ButtonState.all(34.0), foregroundColor: ButtonState.resolveWith((states) => states.isHovering ? Colors.red : Colors.grey.toAccentColor())), onPressed: () => Navigator.pop(context)),
-              Padding(padding: const EdgeInsets.all(40.0), child: IconButton(icon: const Icon(FluentIcons.emoji_neutral), style: ButtonStyle(iconSize: ButtonState.lerp(ButtonStates.disabled, ButtonStates.hovering, 0.5, (p0, p1, p2) => ), foregroundColor: ButtonState.resolveWith((states) => states.isHovering ? Colors.grey : Colors.grey.toAccentColor())), onPressed: () => Navigator.pop(context)),),
-              IconButton(icon: const Icon(FluentIcons.emoji2), style: ButtonStyle(iconSize: ButtonState.all(34.0), foregroundColor: ButtonState.resolveWith((states) => states.isHovering ? Colors.green : Colors.grey.toAccentColor())), onPressed: () => Navigator.pop(context))
-            ],)
+                child: const Text("Back"),
+                onPressed: () => Navigator.pop(context)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AnimatedButton(
+                  controller: _controller1,
+                  status: Status.needsPractise,
+                  onPressed: callback,
+                ),
+                AnimatedButton(
+                    controller: _controller2,
+                    status: Status.okay,
+                    onPressed: callback,
+                    padding: true),
+                AnimatedButton(
+                    controller: _controller3,
+                    status: Status.done,
+                    onPressed: callback)
+              ],
+            )
           ],
         )),
       ),
     );
+  }
+
+  void finishCard() {
+    finishedCards++;
+
+    setState(() {_progress = finishedCards / cards;});
   }
 }
